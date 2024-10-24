e40e0a5b6e75723f2b2124e5dbcb2309887966ad
Luis Augusto Barbosa <luisgamer87enosso@gmail.com>
2024-10-24 15:50:25 +0000

    aluguel_de_carros_atividade_coding

---
+++ aluguel_carros.js
@@ -0,0 +1,8 @@
+let carros = ['Uno', 'Gol', 'Civic', 'Corolla', 'Kwid']
+console.log(carros)
+carros.shift(0)
+console.log(carros)
+carros.unshift('Palio')
+console.log(carros)
+let quantidade_lista = carros.length
+console.log(quantidade_lista)
